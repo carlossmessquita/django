eb50fba3a3e4970bd33f69c82c7ef2855971edb6
carlos_mesquita <carlossmessquita@gmail.com>
2021-10-20 11:26:20 -0300

    Integrando páginas de receitas

--- receitas/views.py
+++ receitas/views.py
@@ -1,23 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import Receita
+
 
 
 def index(request):
 
-    receitas = {
-        1:'Lasanha',
-        2:'Sopa de Legumes',
-        3:'Sorvete',
-        4:'Bolo de Chocolate',
-        5:'Suco Verde',
-        6:'Torta de Limão'
-    }
+    receitas = Receita.objects.all()
+
     dados = {
-        'nome_das_receitas' : receitas
+        'receitas' : receitas
     }
 
     return render(request, 'index.html', dados)
 
 
-def receita(request):
-    return render(request, 'receita.html')
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    receita_a_exibir = {
+        'receita':receita
+
+    }
+    return render(request, 'receita.html', receita_a_exibir)
