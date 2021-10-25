5ff411e057c7bd3171138211f36e864c7307f484
carlos_mesquita <carlossmessquita@gmail.com>
2021-10-25 13:24:42 -0300

    Parte 2 - Desenvolvimento com Django - Alura

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import Receita
 
 
-admin.site.register(Receita)
+# melhoria da interface do django admin
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'publicada')  # mostra detalhes das receitas para edição
+    list_display_links = ('id', 'nome_receita') # cria links para a edição das receitas
+    search_fields = ('nome_receita', ) # cria campo de busca
+    list_filter = ('categoria', ) # cria filtros de pesquisa
+    list_per_page = 5 # paginação
+    list_editable = ('publicada', )
+
+
+admin.site.register(Receita, ListandoReceitas)
 
--- receitas/models.py
+++ receitas/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from datetime import datetime
+from pessoas.models import Pessoa
 
 
 class Receita(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
     modo_de_preparo = models.TextField()
@@ -10,4 +12,9 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now(), blank=True)
+    publicada = models.BooleanField(default=False)
+    foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
+
+    def __str__(self):
+        return self.nome_receita
 
